e4ab924042936d841c58fb490aef3b47236ee200
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-05-25 22:16:11 -0300

    fixing route bug

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Route, BrowserRouter, Switch } from "react-router-dom";
 import Landing from "./views/Landing";
 import Home from "./views/Customer/Home";
 import Cart from "./views/Customer/Cart";
@@ -8,14 +8,16 @@ import Status from "components/status/index";
 import Orders from "./views/Customer/Orders";
 
 export default (props) => (
-    <Router>
-        <Route path="/" component={Landing} />
-        <Route path="/home" component={Home} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/orders" component={Orders} />
-        <Route path="*" >
-            <Status text="Página não encontrada..." icon="fa fa-exclamation fa-100x" />
-        </Route>
-    </Router>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={Landing} />
+            <Route path="/home" component={Home} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="*" >
+                <Status text="Página não encontrada..." icon="fa fa-exclamation fa-100x" />
+            </Route>
+        </Switch>
+    </BrowserRouter>
 );
