2c86524dbb517b5dab491a2aed37c5d1277a7576
LioCroons <liomanjate@gmail.com>
2020-06-16 20:05:57 -0300

    TelasProjectos

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,6 +8,12 @@ import Status from "./views/Customer/Status/index";
 import Orders from "./views/Customer/Orders/index";
 import Order from "./views/Customer/Order/index";
 import OrderStatus from "./views/Customer/OrderStatus/index";
+import Login from "./components/Form/Login/index";
+import Signup from "./components/Form/Sign-up/index";
+import ListaProdutos from "./components/ProductList/index";
+import Entregador from "./components/Form/Entregador/index";
+import Recuperar from "./components/Form/Recuperarsenha/index";
+import  "./app.css";
 
 export default (props) => (
     <BrowserRouter>
@@ -15,12 +21,18 @@ export default (props) => (
             <Switch>
                 <Route path="/" exact={true} component={Landing} />
                 <Route path="/home" component={Home} />
+                <Route path="/auth/Login" component={Login} />
+                <Route path="/resister/Signup" component={Signup} />
+                <Route pacth="/senha/recuperar" component={Recuperar} />
                 <Route path="/cart" component={Cart} />
+                <Route path="/products/ListaProdutos" component={ListaProdutos} />
                 <Route path="/checkout" component={Checkout} />
                 <Route path="/orders" component={Orders} />
                 <Route path="/order/status" component={OrderStatus} />
                 <Route path="/order" component={Order} />
+                <Route path="/p/entregador" component={Entregador} />
                 <Route path="*" component={Status} />
+                
             </Switch>
         </div>
     </BrowserRouter>
